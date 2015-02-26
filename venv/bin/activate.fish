00b6416c44d808306c6ddfce52669a561969a133
bojia <bo.jia@ihandysoft.com>
2015-02-26 19:08:38 +0800

    none

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/sharp/Workspace/python/Pycharm/iflyshadow/venv"
+set -gx VIRTUAL_ENV "/Users/sharp/Python/workspace/getFollow/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
